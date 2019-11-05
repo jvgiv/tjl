666bdbe74d2736cb20758c7aed34aef84e5d79ac
jvgiv <jvgiv12@gmail.com>
2019-11-05 13:04:14 -0500

    Form being built for contact page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react'
 
 export default class ContactUs extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             name: '',
+             email: '',
+             subject: '',
+             message: ''
+
+        }
+    }
+
+    handleChange = (e) => {
+        e.preventDefault()
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+    
     render() {
+        console.log(this.state.name)
         return (
             <div>
-                
+                <div>
+
+                </div>
+                <form>
+                    <input
+                        type='text'
+                        name='name'
+                        placeholder='Name'
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type='text'
+                        name='email'
+                        placeholder='Email'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type='text'
+                        name='subject'
+                        placeholder='Subject'
+                        value={this.state.subject}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type='text'
+                        name='message'
+                        placeholder='Type your message here:'
+                        value={this.state.message}
+                        onChange={this.handleChange}
+                    />
+                </form>
             </div>
         )
     }
