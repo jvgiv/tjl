0268ff97a76c100d86c0b92c036e6b8bdcbf2c91
jvgiv <jvgiv12@gmail.com>
2019-11-06 11:13:50 -0500

    pics uploaded, content fixed, wording edited on BB

--- src/components/BrandBuilders.js
+++ src/components/BrandBuilders.js
@@ -4,7 +4,21 @@ export default class BrandBuilders extends Component {
     render() {
         return (
             <div>
-                
+                <h2>Brand Builders</h2>
+                <div className='bb-container'>
+                    <p className='bb-container-children'>Strategic Aspirational Product Development</p>
+                    <p className='bb-container-children'>US Delivery Chain Importation Knowledge</p>
+                    <p className='bb-container-children'>Product, People, Promotion</p>
+                    <p className='bb-container-children'>Product Seminars</p>
+                    <p className='bb-container-children'>Price Strategy for Growth Opportunities</p>
+                    <p className='bb-container-children'>Brand Ambassador Programs</p>
+                    <p className='bb-container-children'>Seasonal Reward Programs</p>
+                    <p className='bb-container-children'>Curating for US Markets</p>
+                    <p className='bb-container-children'>Trunk Show Expertise</p>
+                    <p className='bb-container-children'></p>
+                    <p className='bb-container-children'>Seasonal Forecast</p>
+                    <p className='bb-container-children'></p>
+                </div>
             </div>
         )
     }
--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -4,7 +4,10 @@ export default class OurStory extends Component {
     render() {
         return (
             <div>
-                
+                <h2>Our Story</h2>
+                <p>The TJ Luxury Group partnership began in 2000 when Tracy Welch hired James Fletcher as a Sales Executive for the Rena Lange Couture collection. After 15 years the two formed TJ Luxury Group, quickly establishing themselves as premier brand builders of emerging talent.</p>
+                <p>Working with the finest retailers in over 50 doors our experience in catering to and serving the end consumer is supreme. </p>
+                <p>TJ Luxury Group seeks to partner with luxury brands and designers passionate about product and placement in the North American market.</p>
             </div>
         )
     }
