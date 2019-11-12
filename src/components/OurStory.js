b2b4630c3cf544f4fdb2cb2dc2c4c0e79d27f55f
jvgiv <jvgiv12@gmail.com>
2019-11-12 18:19:08 -0500

    Parallax added to the story

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -4,6 +4,8 @@ import { Parallax } from 'react-parallax';
 import Footer from './Footer';
 
 const image = 'https://w.wallhaven.cc/full/0j/wallhaven-0jgmvw.png';
+const image2 = 'https://w.wallhaven.cc/full/0q/wallhaven-0qmgrq.jpg';
+const image3 = 'https://w.wallhaven.cc/full/j8/wallhaven-j8k1xw.jpg';
 
 export default class OurStory extends Component {
   render() {
@@ -16,9 +18,9 @@ export default class OurStory extends Component {
         </Parallax>
         <div className='our-story-container'>
           <h3>Our Story</h3>
-          <div className='our-story-containers'>
-            <div className='story-child-1'>
-            <div className='o-s-c-child-1'>
+     {/* One */}
+             <div className='story-child-1'>
+          <div className='o-s-c-child-1'>
               <p>
                 The TJ Luxury Group partnership began in 2000 when Tracy Welch hired
                 James Fletcher as a Sales Executive for the Rena Lange Couture
@@ -27,28 +29,44 @@ export default class OurStory extends Component {
                 talent.
               </p>
             </div>
-            <div className='o-s-c-child-2'>
-              <h1>19 Years</h1>
-              <h4>Catering To You</h4>
-            </div>
+               <div className='o-s-c-child-2'>
+                 <Parallax style={{width: '100%'}} bgImage={image2} strength={500}>
+          <div className='story-parallax-bg'>
+            <h1>19 Years</h1>
+            <h4 >Catering To You.</h4> 
           </div>
-          </div>
-          <div className='story-child-2'>
-          <div className='our-story-containers'>
-            <div  className='o-s-c-child-2'>
-              <h1>Retail Professionals</h1>
+        </Parallax>
             </div>
-            <div  className='o-s-c-child-1'>
+            </div>
+
+            {/* two */}
+
+               
+             <div className='story-child-2'>
+               <div className='o-s-c-child-2'>
+                 <Parallax style={{width: '100%'}} bgImage={image3} strength={500}>
+          <div className='story-parallax-bg'>
+            <h1>19 Years</h1>
+            <h4 >Catering To You.</h4> 
+          </div>
+        </Parallax>
+            </div>
+                      <div className='o-s-c-child-1'>
               <p>
-                We work with the finest retailers in over 50 doors.  We excel when it comes to serving the consumer's needs.
+                The TJ Luxury Group partnership began in 2000 when Tracy Welch hired
+                James Fletcher as a Sales Executive for the Rena Lange Couture
+                collection. After 15 years the two formed TJ Luxury Group, quickly
+                establishing themselves as premier brand builders of emerging
+                talent.
               </p>
             </div>
-          </div>
-          </div>
+            </div>
+
           
-        </div>
-        {/* <Footer/> */}
-        
+
+          </div>
+
+      
       </>
     );
   }
